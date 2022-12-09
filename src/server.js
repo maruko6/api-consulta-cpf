2780c26ac59ffca147f134f4089e7d0c66b944fe
Marcone-Santo1 <marconemagalhaes203@gmail.com>
2022-12-09 15:37:33 -0300

    add *

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "../index.js";
 
-const port = 80;
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Iniciando o server https://gorgeous-babka-40641c.netlify.app:${port}`);
+    console.log(`Iniciando o server localhost:${port}`);
 })
